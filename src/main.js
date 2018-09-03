b9c3bf5cedbfee08177572c2f940f036a8da6943
wangyongzhiyjm <1459131395@qq.com>
2018-09-03 12:04:57 +0800

    第一次

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import elementui from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(elementui);
 // 开发提示
 Vue.config.productionTip = false;
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+
+
 
 Vue.use(Router);
+// @scr文件夹
+// 导入组件
+import login from '@/views/login.vue'
 
 export default new Router({
   routes: [
-    { path: '/', name: 'HelloWorld', component: HelloWorld }
+    {name:'登陆',path:'/login',component:login}
   ]
 });
